96366fe2e71810291ce4544eed0266964d12acea
Christian Kemper <christian.kemper@me.com>
2023-03-16 18:23:22 +0000

    adding different output formats

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use serde_json;
+use serde_yaml;
 use ulid::Ulid;
 
 /// Simple program to generate ULIDs
@@ -8,13 +10,35 @@ struct Args {
     /// Number of ULIDs to generate
     #[arg(short, long, default_value_t = 1)]
     count: u32,
+
+    /// Type of output to print: string, json, yaml
+    #[arg(short, long, default_value = "string")]
+    output_format: String,
 }
 
 fn main() {
     let args = Args::parse();
 
+    let mut ulids: Vec<String> = Vec::new();
+
     for _ in 0..args.count {
-        let ulid = Ulid::new();
-        println!("{}", ulid.to_string());
+        ulids.push(Ulid::new().to_string());
+    }
+
+    match args.output_format.as_str() {
+        "string" => {
+            for ulid in ulids {
+                println!("{}", ulid);
+            }
+        }
+        "json" => {
+            println!("{}", serde_json::to_string(&ulids).unwrap());
+        }
+        "yaml" => {
+            println!("{}", serde_yaml::to_string(&ulids).unwrap());
+        }
+        _ => {
+            println!("Invalid output format");
+        }
     }
 }
